db86cecc6b7f76f86268d04c916a79d31fd3c90a
Nazmul hossain Adnan <tahsin10@outlook.com>
2025-12-26 21:55:38 +0600

    fixed build issue

--- components/ErrorNotification.tsx
+++ components/ErrorNotification.tsx
@@ -102,7 +102,7 @@ export default function ErrorNotification({
               </p>
 
               {/* Show technical details for transparency */}
-              {error.statusCode && (
+              {'statusCode' in error && error.statusCode && (
                 <p className={`text-xs mt-1 ${getTextColor(error.type)} opacity-60`}>
                   Error code: {error.statusCode}
                 </p>
